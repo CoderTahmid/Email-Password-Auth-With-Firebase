ad581066ce2d49849a77ea012bd4ecf64eb03378
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-03 09:26:23 +0600

    showed how to get email data from the form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,7 +4,7 @@ const Register = () => {
 
     const handleRegister = (event) => {
         event.preventDefault();
-        console.log("please register");
+        console.log(event.target.email.value);
     }
 
     return (
@@ -24,7 +24,7 @@ const Register = () => {
                             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                         </g>
                     </svg>
-                    <input type="email" placeholder="Email" required />
+                    <input type="email" name='email' placeholder="Email" required />
                 </label>
                 <div className="validator-hint hidden">Enter valid email address</div>
                 <label className="input validator">
